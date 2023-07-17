3ee70274e90b8495aa26b8ed9e500862d8da7ba2
ranupatidar05 <ranupatidar4589@gmail.com>
2023-07-17 12:21:41 +0530

    new

--- js/script3.js
+++ js/script3.js
@@ -15,13 +15,16 @@ console.log(a);
 //object.member
 console.log(a.length);
 
-let b = [...a,"krishti",'krtika'];
+let b = [...a,"krishti",'krtika',];
+let b1 = [...a,"krishti",'krtika',...a];
 console.log(b);
 console.log(b.length);
 
 var paragraf=''
 for(var i=0; i<b.length;i++){
     console.log(b[i]);
-    paragraf = paragraf + b[i]+' ';
+    //paragraf = paragraf + b[i]+' '; //not optimized slow
+    paragraf += b[i]+' '; // optmized fast
+    paragraf += b1[i]+' '; // optmized fast
 }
 console.log(paragraf);
